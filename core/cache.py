fa43ffabfd3231472065f6c47cf26610a59d31c1
Yorick Chollet <yorick.chollet@gmail.com>
2014-10-22 13:43:58 -0600

    implemented cache

--- core/cache.py
+++ core/cache.py
@@ -1,1 +1,67 @@
 __author__ = 'Yorick Chollet'
+
+from dateutil.relativedelta import relativedelta
+import tgutils
+
+from handler import validate_response
+
+from dogpile.cache import make_region
+
+import logging
+
+
+# cache expiration TODO push to constants
+TEN_DAYS = 3600 * 24 * 10
+
+
+class Cache:
+
+    def __init__(self, tolerance=0, enabled=True):
+        self.tolerance = relativedelta(seconds=tolerance)
+        self.enabled = enabled
+        #TODO pass in configure
+        self.backend = make_region().configure(
+            'dogpile.cache.dbm',
+            expiration_time=TEN_DAYS,
+            arguments={
+                'filename': 'core/cache_data',
+                'rw_lockfile': 'core/cache_rwlock',
+                'dogpile_lockfile': 'core/cache_dogpilelock'
+            }
+        )
+
+    def get_until(self, memento, date, getter, *args, **kwargs):
+        if not self.enabled:
+            return getter(*args, **kwargs)
+
+        # No need to refresh if table young enough
+        val = self.backend.get(memento)
+        age = 0
+        timemap = None
+
+        if val:
+            age = val[0]
+            timemap = val[1]
+            logging.info("Cached value exists for %s" % memento)
+        else:
+            logging.info("No cached value for %s" % memento)
+
+
+        if not val or date > age + self.tolerance:
+            # Cache possibly outdated
+            (uri, timemap) = validate_response(getter(*args, **kwargs))
+            self.set(memento, timemap)
+
+        return timemap
+
+    def get_all(self, memento, getter, *args, **kwargs):
+        # No way to know if table is new other than retrieve one.
+        # Hope to retrieve in the tolerance delta.
+        return self.get_until(memento, tgutils.now(), getter, *args, **kwargs)
+
+    def set(self, memento, timemap):
+        logging.info("Updating cache for %s" % memento)
+        age = tgutils.now()
+        val = (age, timemap)
+        key = memento
+        return self.backend.set(key, val)
