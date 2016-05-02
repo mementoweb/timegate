032dc827868c2b5d2f00b32fbf8993eed22ba16c
Luda171 <ludab@lanl.gov>
2016-05-02 16:52:02 +0000

    added cat aueb sg

---
+++ core/handler_examples/cat.py
@@ -0,0 +1,46 @@
+
+import urllib
+import logging
+
+import re
+
+from core.handler_baseclass import Handler
+from errors.timegateerrors import HandlerError
+
+
+__author__ = "aalsum, Yorick, Luda"
+
+
+class CataloniaHandler(Handler):
+
+    def __init__(self):
+        
+        self.baseuri = "http://www.padi.cat:8080/wayback/*/"
+      
+        regex = r'<a onclick="SetAnchorDate\(\'.*\'\);" href="http://www.padi.cat:8080/wayback/[\S]*">';
+        self.uriRegex = re.compile(regex)
+        Handler.__init__(self)
+
+    def get_all_mementos(self, req_url):
+        # def fetch_changes(self, req, requri, dt=None):
+        # implement the changes list for this particular proxy
+
+        uri = self.baseuri + req_url
+        try:
+            fh = urllib.urlopen(uri)
+        except Exception as e:
+            logging.error("Couldn't retrieve data from %s : %s" % (uri, str(e)))
+            return None
+        data = fh.read()
+        fh.close()
+
+        changes = []
+        uris = re.findall(self.uriRegex, data)
+        for u in uris:
+            dtstr = u[27:41]
+            loc = u[52:-2]
+            dtstr += " GMT"
+            # dtobj = dateparser.parse(dtstr)
+            changes.append((loc, dtstr))
+
+        return changes
